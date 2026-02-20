9b45eac2b19531303b63c923150e4266553975d0
Giacomo Folli <giacomo.folli@studenti.unipr.it>
2026-02-20 21:36:53 +0100

    add privacy policy page with minimal styling

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Privacy Policy</h1>
+<p>Visual-Planner requires access to your Google account for the following purposes:</p>
+<ul>
+	<li>View your calendars to display your events in a year-view format for better planning.</li>
+	<li>
+		Manage its own configuration data in your Google Drive to save your app settings across sessions
+		and devices.
+	</li>
+</ul>
+<p>
+	Visual-Planner does not transfer, store, or share your personal data or any other information.
+</p>
+
+<svelte:head>
+    <title>visual-planner | Privacy Policy</title>
+</svelte:head>
+
+<style>
+	/* Minimal styling for the privacy policy page */
+	h1 {
+		font-size: 1.5rem;
+		margin-bottom: 1rem;
+	}
+
+	p {
+		margin-bottom: 1rem;
+		line-height: 1.5;
+	}
+</style>
